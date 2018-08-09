fc36bf685738d2452da79bef2d97edf75b1a68e7
Ablakes <blakesley.alex@gmail.com>
2018-08-09 13:31:36 -0700

    incorporated timer adjusters

--- js/main.js
+++ js/main.js
@@ -1,47 +1,107 @@
-const timer = document.getElementById('timer');
-const start = document.getElementById('start');
-const reset = document.getElementById('reset');
-const rest = document.getElementById('rest');
-const end = document.getElementById('end');
+const UIController = (function () {
 
-var watch = new Stopwatch(timer);
-var rester = new Stopwatch(rest);
+    const timer = document.getElementById('timer');
+    const start = document.getElementById('start');
+    const rest = document.getElementById('rest');
+    const end = document.getElementById('end');
+    const workList = document.querySelector(".record__column-heading--work");
+    const restList = document.querySelector(".record__column-heading--rest");
+    const totalList = document.querySelector(".record__column-heading--total");
+    const restAddTime = document.getElementById('btn-rest-add-time');
+    const workAddTime = document.getElementById('btn-work-add-time');
+    const restSubtractTime = document.getElementById('btn-rest-subtract-time');
+    const workSubtractTime = document.getElementById('btn-work-subtract-time');
 
-start.addEventListener('click', () => {
-    if (watch.isOn) {
-        watch.stop();
-        rester.start();
-        start.textContent = "Work";
-        timer.classList.remove('turnGreen');
-        timer.classList.add('turnGrey');
-        timer.style.color = "rgb(60, 102, 52)";
-        timer.style.transform = "scale(.85)";
-        rest.style.color = "rgba(0, 0, 0)";
-        rest.style.transform = "scale(1)";
+    let workClock = new Stopwatch(timer);
+    let breakClock = new Stopwatch(rest);
 
-    } else {
-        watch.start();
-        rester.stop();
-        start.textContent = "Break";
-        timer.classList.add('turnGreen');
-        timer.classList.remove('turnGrey');
-        timer.style.color = "rgb(13, 104, 21)";
-        timer.style.transform = "scale(1)";
-        rest.style.color = "rgba(120, 120, 120)";
-        rest.style.transform = "scale(.85)";
+    restAddTime.addEventListener('click', () => {
+        breakClock.addTime();
+    });
+    workAddTime.addEventListener('click', () => {
+        workClock.addTime();
+    });
+    restSubtractTime.addEventListener('click', () => {
+        breakClock.subtractTime();
+    });
+    workSubtractTime.addEventListener('click', () => {
+        workClock.subtractTime();
+    });
+
+    start.addEventListener('click', () => {
+        if (workClock.isOn) {
+            workClock.stop();
+            breakClock.start();
+            start.textContent = "Work";
+            timer.classList.remove('turnGreen');
+            timer.classList.add('turnGrey');
+            timer.style.color = "rgb(60, 102, 52)";
+            timer.style.transform = "scale(.85)";
+            rest.style.color = "rgba(0, 0, 0)";
+            rest.style.transform = "scale(1)";
+
+        } else {
+            workClock.start();
+            breakClock.stop();
+            start.textContent = "Break";
+            timer.classList.add('turnGreen');
+            timer.classList.remove('turnGrey');
+            timer.style.color = "rgb(13, 104, 21)";
+            timer.style.transform = "scale(1)";
+            rest.style.color = "rgba(120, 120, 120)";
+            rest.style.transform = "scale(.85)";
+
+        }
+    });
+
+    const updateworkClock = () => {
+        const li = document.createElement('li');
+        const time = workClock.returnTime();
+        const formattedworkClock = recordFormatter(time);
+        li.appendChild(document.createTextNode(formattedworkClock));
+        li.className = "record__column-item";
+        li.classList.add('green');
+        workList.appendChild(li);
+    }
+
+    const updateRestTime = () => {
+        const li = document.createElement('li');
+        const restTime = breakClock.returnTime();
+        const formattedRestTime = recordFormatter(restTime);
+        li.className = "record__column-item";
+        li.classList.add('red');
+        li.appendChild(document.createTextNode(formattedRestTime));
+        restList.appendChild(li);
 
     }
-});
 
-reset.addEventListener('click', () => {
-    watch.reset();
-    rester.reset();
-});
+    const updateTotalTime = () => {
+        const li = document.createElement('li');
+        const totalTime = workClock.returnTime() + breakClock.returnTime();
+        const formattedTotalTime = recordFormatter(totalTime);
+        li.appendChild(document.createTextNode(formattedTotalTime));
+        li.className = "record__column-item";
+        totalList.appendChild(li);
 
-end.addEventListener('click', () => {
-    watch.stop();
-    rester.stop();
-    totalTime = watch.returnTime() + rester.returnTime();
-    formattedTotalTime = watch.returnFormatter(totalTime);
-    console.log(formattedTotalTime);
-});
+    }
+
+    end.addEventListener('click', () => {
+        workClock.stop();
+        breakClock.stop();
+        updateworkClock();
+        updateRestTime();
+        updateTotalTime();
+        workClock.reset();
+        breakClock.reset();
+        start.textContent = "Work";
+    });
+
+
+    const recordFormatter = (time) => {
+        var minutes = parseInt((time / (1000 * 60)) % 60),
+            hours = parseInt((time / (1000 * 60 * 60)) % 24);
+        minutes = (minutes < 10) ? "0" + minutes : minutes;
+        return hours + ":" + minutes;
+    }
+
+})();
--- js/stopwatch.js
+++ js/stopwatch.js
@@ -13,18 +13,20 @@ function Stopwatch(elem) {
     };
     const delta = () => {
         //delta just means change
-        var newTime = Date.now();
+        let newTime = Date.now();
         //This gives us a time in milliseconds. We'll calculate the timePassed and pass that into a Date object to gain access to getMinutes() getSeconds() etc. This will make formatting easier. 
-        var timePassed = newTime - offset;
+        let timePassed = newTime - offset;
         offset = newTime;
         return timePassed;
     };
 
     const timeFormatter = (time) => {
-        var milliseconds = parseInt((time % 1000) / 10),
-            seconds = parseInt((time / 1000) % 60),
-            minutes = parseInt((time / (1000 * 60)) % 60),
-            hours = parseInt((time / (1000 * 60 * 60)) % 24);
+        let milliseconds = Math.floor((time % 1000) / 10),
+            seconds = Math.floor((time / 1000) % 60),
+            minutes = Math.floor((time / (1000 * 60)) % 60),
+            hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+        //The modulo makes sure to cut off the hours at 24, minutes at 60, etc. Ex: 12%24 returns 12, whereas 36%24 returns 12 (because it's been 1 day and 12 hours).
+        //Math.floor is important because any number less than 1 means that it hasn't reached the trigger point to add another digit to that denomination. 
 
         hours = (hours < 10) ? "0" + hours : hours;
         minutes = (minutes < 10) ? "0" + minutes : minutes;
@@ -32,34 +34,15 @@ function Stopwatch(elem) {
         milliseconds = (milliseconds < 10) ? "0" + milliseconds : milliseconds;
 
         return hours + " : " + minutes + " : " + seconds + " : " + milliseconds;
-
-
-        //This is the way to do it with the date object v
-
-        // var dateTime = new Date(time);
-        // dateTime.setHours(dateTime.getHours() - 16);
-        // let hours = dateTime.getHours().toString();
-        // let minutes = dateTime.getMinutes().toString();
-        // let seconds = dateTime.getSeconds().toString();
-        // let milliseconds = dateTime.getMilliseconds().toString();
-
-        // hours.length < 2 ? hours = '0' + hours : null;
-        // minutes.length < 2 ? minutes = '0' + minutes : null;
-        // seconds.length < 2 ? seconds = '0' + seconds : null;
-
-        // while (milliseconds.length < 3) {
-        //     milliseconds = `0${milliseconds}`;
-        // }
-
-        // milliseconds = milliseconds.slice(0, -1);
-
-        // return (`${hours} : ${minutes} : ${seconds} : ${milliseconds}`);
-
-
     };
 
-    this.returnFormatter = (time) => {
-        return timeFormatter(time);
+    this.addTime = () => {
+        time += 300000;
+        update();
+    }
+    this.subtractTime = () => {
+        time -= 300000;
+        update();
     }
 
     this.isOn = false;
@@ -88,3 +71,28 @@ function Stopwatch(elem) {
         update();
     };
 }
+
+
+
+
+
+//This is the way to format time with the date object v
+
+// var dateTime = new Date(time);
+// dateTime.setHours(dateTime.getHours() - 16);
+// let hours = dateTime.getHours().toString();
+// let minutes = dateTime.getMinutes().toString();
+// let seconds = dateTime.getSeconds().toString();
+// let milliseconds = dateTime.getMilliseconds().toString();
+
+// hours.length < 2 ? hours = '0' + hours : null;
+// minutes.length < 2 ? minutes = '0' + minutes : null;
+// seconds.length < 2 ? seconds = '0' + seconds : null;
+
+// while (milliseconds.length < 3) {
+//     milliseconds = `0${milliseconds}`;
+// }
+
+// milliseconds = milliseconds.slice(0, -1);
+
+// return (`${hours} : ${minutes} : ${seconds} : ${milliseconds}`);
