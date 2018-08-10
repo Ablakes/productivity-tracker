eeb48530f48e5395718ccdd274fc560abc764496
Ablakes <blakesley.alex@gmail.com>
2018-08-10 13:51:19 -0700

    {

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const UIController = (function () {
 
-    const timer = document.getElementById('timer');
+    const workClock = document.getElementById('work-clock');
     const start = document.getElementById('start');
     const rest = document.getElementById('rest');
     const end = document.getElementById('end');
@@ -13,8 +13,10 @@ const UIController = (function () {
     const restSubtractTime = document.getElementById('btn-rest-subtract-time');
     const workSubtractTime = document.getElementById('btn-work-subtract-time');
 
-    let workTime = new Stopwatch(timer);
+    let workTime = new Stopwatch(workClock);
     let breakClock = new Stopwatch(rest);
+
+    const data = [];
 
     let formattedDate;
 
@@ -36,87 +38,18 @@ const UIController = (function () {
             workTime.stop();
             breakClock.start();
             start.textContent = "Work";
-            timer.classList.remove('turnGreen');
-            timer.classList.add('turnGrey');
-            timer.style.color = "rgb(60, 102, 52)";
-            timer.style.transform = "scale(.85)";
-            rest.style.color = "rgba(0, 0, 0)";
-            rest.style.transform = "scale(1)";
+            animateworkClockOn();
+            animateBreakClockOff();
 
         } else {
             workTime.start();
             breakClock.stop();
             start.textContent = "Break";
-            timer.classList.add('turnGreen');
-            timer.classList.remove('turnGrey');
-            timer.style.color = "rgb(13, 104, 21)";
-            timer.style.transform = "scale(1)";
-            rest.style.color = "rgba(120, 120, 120)";
-            rest.style.transform = "scale(.85)";
+            animateworkClockOff();
+            animateBreakClockOn();
 
         }
     });
-
-    const data = {};
-
-    const pushToDateRecord = () => {
-
-        //Add to record list
-        const date = new Date();
-        const month = date.getMonth();
-        const day = date.getDate();
-        formattedDate = `${month}/${day}`;
-        const li = document.createElement('li');
-        li.className = 'record__column-item';
-        li.appendChild(document.createTextNode(formattedDate));
-        dateList.appendChild(li);
-
-        //Push to data
-        formattedDate = formattedDate;
-        data[formattedDate] = [];
-        return formattedDate;
-    }
-
-    const pushToWorkRecord = () => {
-        const li = document.createElement('li');
-        const time = workTime.returnTime();
-        const formattedWorkTime = recordFormatter(time);
-        li.appendChild(document.createTextNode(formattedWorkTime));
-        li.className = "record__column-item";
-        li.classList.add('green');
-        workList.appendChild(li);
-
-        //push to data
-        data[formattedDate].push(formattedWorkTime);
-    }
-
-    const pushToBreakRecord = () => {
-        const li = document.createElement('li');
-        const restTime = breakClock.returnTime();
-        const formattedRestTime = recordFormatter(restTime);
-        li.className = "record__column-item";
-        li.classList.add('red');
-        li.appendChild(document.createTextNode(formattedRestTime));
-        restList.appendChild(li);
-
-        //push to data
-        data[formattedDate].push(formattedRestTime);
-    }
-
-    const pushToTotalRecord = () => {
-        const li = document.createElement('li');
-        const totalTime = workTime.returnTime() + breakClock.returnTime();
-        const formattedTotalTime = recordFormatter(totalTime);
-        li.appendChild(document.createTextNode(formattedTotalTime));
-        li.className = "record__column-item";
-        totalList.appendChild(li);
-
-        //push data to local storage
-        data[formattedDate].push(formattedTotalTime);
-        const dateRecord = Object.keys(data)[0];
-        const timeRecord = JSON.stringify(data[dateRecord]);
-        localStorage.setItem(dateRecord, timeRecord);
-    }
 
     end.addEventListener('click', () => {
         workTime.stop();
@@ -130,6 +63,106 @@ const UIController = (function () {
         start.textContent = "Work";
     });
 
+    const animateworkClockOn = () => {
+        workClock.classList.remove('turnGreen');
+        workClock.classList.add('turnGrey');
+        workClock.style.color = "rgb(60, 102, 52)";
+        workClock.style.transform = "scale(.85)";
+    }
+
+    const animateworkClockOff = () => {
+        workClock.classList.add('turnGreen');
+        workClock.classList.remove('turnGrey');
+        workClock.style.color = "rgb(13, 104, 21)";
+        workClock.style.transform = "scale(1)";
+    }
+
+    const animateBreakClockOn = () => {
+        rest.style.color = "rgba(120, 120, 120)";
+        rest.style.transform = "scale(.85)";
+    }
+
+    const animateBreakClockOff = () => {
+        rest.style.color = "rgba(0, 0, 0)";
+        rest.style.transform = "scale(1)";
+    }
+
+    const postDataItem = (item, list, className) => {
+        const li = document.createElement('li');
+        li.className = 'record__column-item';
+        li.appendChild(document.createTextNode(item));
+        list.appendChild(li);
+        li.classList.add(className);
+    }
+
+    const loadData = () => {
+
+        //re-populate data from localStorage
+        Object.keys(localStorage).forEach((item) => {
+            data.push(JSON.parse(localStorage.getItem(item)));
+        });
+
+        // re-post data to records
+        data.forEach((item) => {
+            postDataItem(item[0], dateList);
+            postDataItem(item[1], workList, 'green');
+            postDataItem(item[2], restList, 'red');
+            postDataItem(item[3], totalList);
+        })
+    };
+
+    loadData();
+
+    const pushToDateRecord = () => {
+
+        //Add to record list
+        const date = new Date();
+        const month = date.getMonth();
+        const day = date.getDate();
+        formattedDate = `${month}/${day}`;
+
+        postDataItem(formattedDate, dateList);
+
+        //Push to data
+        data.push([]);
+        data[data.length - 1].push(formattedDate);
+    }
+
+    const pushToWorkRecord = () => {
+        const time = workTime.returnTime();
+        const formattedWorkTime = recordFormatter(time);
+
+        postDataItem(formattedWorkTime, workList, 'green');
+
+        //push to data
+        data[data.length - 1].push(formattedWorkTime);
+    }
+
+    const pushToBreakRecord = () => {
+        const restTime = breakClock.returnTime();
+        const formattedRestTime = recordFormatter(restTime);
+
+        postDataItem(formattedRestTime, restList, 'red');
+
+        //push to data
+        data[data.length - 1].push(formattedRestTime);
+    }
+
+    const pushToTotalRecord = () => {
+
+        const totalTime = workTime.returnTime() + breakClock.returnTime();
+        const formattedTotalTime = recordFormatter(totalTime);
+
+        postDataItem(formattedTotalTime, totalList);
+
+        //push data to data
+        data[data.length - 1].push(formattedTotalTime);
+
+        //push to local storage
+        let storageKey = (data.length - 1).toString();
+        let storageArr = JSON.stringify(data[storageKey]);
+        localStorage.setItem(storageKey, storageArr)
+    }
 
     const recordFormatter = (time) => {
         var minutes = parseInt((time / (1000 * 60)) % 60),
